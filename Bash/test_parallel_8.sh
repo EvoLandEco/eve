4b179dcf2d63932e79bf5b49957fead9a24cb490
EvoLandEco <tianjianqin@outlook.com>
2021-09-30 15:03:19 +0200

    Change CPUs per task

--- Bash/test_parallel_8.sh
+++ Bash/test_parallel_8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=8:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --job-name=test_parallel
 #SBATCH --output=logs/test_parallel-%j.log
 #SBATCH --mem=10GB
@@ -9,4 +9,6 @@
 
 ml R
 
-Rscript ~/eve/Script/test_parallel.R
+test_name=$1
+
+Rscript ~/eve/Script/test_parallel.R ${test_name}
