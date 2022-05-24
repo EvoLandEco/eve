e46598f413e0c4926135ac39a662b1d0f59f686d
Tianjian <tianjianqin@outlook.com>
2022-05-24 15:35:54 +0200

    New simulation settings

--- Script/run_edd_sim.R
+++ Script/run_edd_sim.R
@@ -5,32 +5,40 @@ set <- as.numeric(args[2])
 nrep <- as.numeric(args[3])
 
 combo1 <- eve::edd_combo_maker(
-  la = c(0.5),
-  mu = c(0.1),
+  la = c(0.5, 0.8),
+  mu = c(0.1, 0.2),
   beta_n = c(-0.01),
-  beta_phi = c(0.02),
-  gamma_n = c(0.01),
-  gamma_phi = c(-0.05),
+  beta_phi = c(-0.02, 0.02),
   age = c(15),
-  model = "dsde2",
+  model = "dsce2",
+  metric = c("pd"),
+  offset = c("none", "simtime", "spcount", "both")
+)
+
+combo2 <- eve::edd_combo_maker(
+  la = c(0.5, 0.8),
+  mu = c(0.1, 0.2),
+  beta_n = c(0),
+  beta_phi = c(-0.02, 0.02),
+  age = c(15),
+  model = "dsce2",
+  metric = c("pd"),
+  offset = c("none", "simtime")
+)
+
+combo3 <- eve::edd_combo_maker(
+  la = c(0.5, 0.8),
+  mu = c(0.1, 0.2),
+  beta_n = c(0, -0.01),
+  beta_phi = c(-0.02, 0.02),
+  age = c(15),
+  model = "dsce2",
   metric = c("ed"),
   offset = c("none")
 )
 
-combo2 <- eve::edd_combo_maker(
-  la = c(0.5),
-  mu = c(0.1),
-  beta_n = c(0, -0.01),
-  beta_phi = c(0.01),
-  gamma_n = c(0, 0.01),
-  gamma_phi = c(-0.005),
-  age = c(15),
-  model = "dsde2",
-  metric = c("pd"),
-  offset = c("none")
-)
+combo <- c(combo1, combo2, combo3)
 
-combo <- c(combo1, combo2)
 names(combo) <- as.character(seq(1, length(combo)))
 
 out <- eve:::edd_sim_rep(combo = combo[[set]], nrep = nrep)
