6df7d8d082622b1b77dd2c6ec359e4964ad024e2
Tianjian <tianjianqin@outlook.com>
2022-05-28 01:44:08 +0200

    check parallel arguments

--- R/parallel.R
+++ R/parallel.R
@@ -1,20 +1,26 @@
-check_parallel_arguments <- function(workers = NULL, strategy = NULL) {
+check_parallel_arguments <- function(strategy = NULL,
+                                     workers = NULL,
+                                     verbose = TRUE) {
   if (workers == 1 |
       identical(strategy, "sequential")) {
-    message("Running sequential data loading")
+    if (verbose == TRUE) {
+      message("Running sequential task")
+    }
     strategy <- eval(parse(text = paste0("future::", strategy)))
     future::plan(strategy)
   } else if (!(workers %% 1 == 0)) {
     stop("number of workers should be an integer")
   } else {
     if (strategy %in% c("multisession", "multicore", "multiprocess")) {
-      message(paste0(
-        "Running ",
-        strategy,
-        " data loading with ",
-        workers,
-        " workers"
-      ))
+      if (verbose == TRUE) {
+        message(paste0(
+          "Running ",
+          strategy,
+          " task with ",
+          workers,
+          " workers"
+        ))
+      }
       strategy <- eval(parse(text = paste0("future::", strategy)))
       future::plan(strategy, workers = workers)
     } else {
