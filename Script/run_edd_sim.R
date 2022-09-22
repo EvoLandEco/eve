f33419fa02a2e759263b68b30f7a5e26fab2c658
Tianjian <tianjianqin@outlook.com>
2022-09-23 01:50:34 +0200

    Add nnd jobs

--- Script/run_edd_sim.R
+++ Script/run_edd_sim.R
@@ -26,7 +26,18 @@ combo2 <- eve::edd_combo_maker(
   offset = c("none")
 )
 
-combo <- c(combo1, combo2)
+combo3 <- eve::edd_combo_maker(
+  la = c(0.5, 0.8),
+  mu = c(0, 0.1, 0.2),
+  beta_n = c(-0.02, -0.01, 0),
+  beta_phi = c(-0.05, -0.02, 0, 0.0005, 0.001),
+  age = c(6),
+  model = "dsce2",
+  metric = c("nnd"),
+  offset = c("none")
+)
+
+combo <- c(combo1, combo2, combo3)
 
 names(combo) <- as.character(seq(1, length(combo)))
 
