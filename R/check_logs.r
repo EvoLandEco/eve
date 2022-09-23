3e7d5acdf4fbf9f472d1eb644e3a9558a1ab6acc
Tianjian <tianjianqin@outlook.com>
2022-09-23 17:24:17 +0200

    implement job_parameter function, along with some improvements

--- R/check_logs.r
+++ R/check_logs.r
@@ -19,15 +19,15 @@ job_status_parser <- function(file = NULL) {
 #' @title Parsing log files in a folder into a matrix of job status
 #' @description Function to parse outputs in all log files of a folder, then
 #' return a matrix to show the status of the jobs
-#' @param path path to the folder containing the log files
+#' @param path path to the result folder
 #' @param which specify status to be filtered
 #' @author Tianjian Qin
 #' @export job_status
 job_status <- function(path = NULL, which = NULL) {
   if (dir.exists(path)) {
-    logs <- list.files(path)
+    file_names <- list.files(file.path(path, "logs"))
     status <-
-      lapply(X = paste0(path, logs),
+      lapply(X = file.path(path, "logs", file_names),
              FUN = job_status_parser)
   } else {
     stop("Invalid path")
@@ -79,14 +79,15 @@ job_detail_parser <- function(file = NULL) {
 #' @title Parsing log files in a folder into a list of job details
 #' @description Function to parse job details in all log files of a folder, then
 #' return a list storing those information
-#' @param path path to the folder containing the log files
+#' @param path path to the result folder
+#' @param which specify which job to be parsed
 #' @author Tianjian Qin
 #' @export job_detail
 job_detail <- function(path = NULL, which = NULL) {
   if (dir.exists(path)) {
-    logs <- list.files(path)
+    file_names <- list.files(file.path(path, "logs"))
     detail <-
-      lapply(X = paste0(path, logs),
+      lapply(X = file.path(path, "logs",file_names),
              FUN = job_detail_parser)
   } else {
     stop("Invalid path")
@@ -96,5 +97,24 @@ job_detail <- function(path = NULL, which = NULL) {
       return(detail)
   } else {
       return(detail[[which]])
+  }
+}
+
+
+
+#' @name job_parameter
+#' @title Find out which parameter set is used for a job
+#' @description Function to find out which parameter set is used for a job
+#' @param path path to the result folder
+#' @param which specify which job to be parsed
+#' @author Tianjian Qin
+#' @export job_detail
+job_parameter <- function(path = NULL, which = NULL) {
+  params <- read.table(file.path(path, "params"))
+
+  if (!is.null(which)) {
+    return(params[which, ])
+  } else {
+    return(params)
   }
 }
