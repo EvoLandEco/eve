ded0674edacfd64f7a915441de1363d7bb1d45fc
Tianjian <tianjianqin@outlook.com>
2022-09-24 14:39:36 +0200

    Switch back to sequential loading

--- Bash/run_edd_load.sh
+++ Bash/run_edd_load.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name=edd_load
 #SBATCH --output=logs/edd_load-%j.log
-#SBATCH --mem=300GB
-#SBATCH --partition=himem
+#SBATCH --mem=32GB
+#SBATCH --partition=regular
 
 name=${1}
 
