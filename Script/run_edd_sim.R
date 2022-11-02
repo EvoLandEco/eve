5c70ddc625a9c5eb775769caec1b4ffdd780c7c6
Tianjian <tianjianqin@outlook.com>
2022-11-02 01:06:08 +0100

    adjust parameters

--- Script/run_edd_sim.R
+++ Script/run_edd_sim.R
@@ -5,10 +5,10 @@ set <- as.numeric(args[2])
 nrep <- as.numeric(args[3])
 
 combo1 <- eve::edd_combo_maker(
-  la = c(0.4, 0.6),
+  la = c(0.5, 0.8),
   mu = c(0, 0.1, 0.2),
   beta_n = c(-0.04, -0.01, 0),
-  beta_phi = c(-0.08, -0.04, 0, 0.002, 0.004),
+  beta_phi = c(-0.1, -0.05, 0, 0.001, 0.002),
   age = c(6),
   model = "dsce2",
   metric = c("pd"),
@@ -16,10 +16,10 @@ combo1 <- eve::edd_combo_maker(
 )
 
 combo2 <- eve::edd_combo_maker(
-  la = c(0.4, 0.6),
+  la = c(0.5, 0.8),
   mu = c(0, 0.1, 0.2),
   beta_n = c(-0.04, -0.01, 0),
-  beta_phi = c(-0.08, -0.04, 0, 0.002, 0.004),
+  beta_phi = c(-0.1, -0.05, 0, 0.001, 0.002),
   age = c(6),
   model = "dsce2",
   metric = c("ed"),
@@ -27,10 +27,10 @@ combo2 <- eve::edd_combo_maker(
 )
 
 combo3 <- eve::edd_combo_maker(
-  la = c(0.4, 0.6),
+  la = c(0.5, 0.8),
   mu = c(0, 0.1, 0.2),
   beta_n = c(-0.04, -0.01, 0),
-  beta_phi = c(-0.08, -0.04, 0, 0.002, 0.004),
+  beta_phi = c(-0.1, -0.05, 0, 0.001, 0.002),
   age = c(6),
   model = "dsce2",
   metric = c("nnd"),
